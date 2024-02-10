9b357fb0f7eb9d960e336657332c0b2e28921fb8
Paulo Victor <paulovictor.devs@gmail.com>
2024-02-10 19:16:44 -0300

    add swagger

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -9,19 +9,23 @@ import {
 } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { Task } from './task.entity';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 
 @Controller('tasks')
+@ApiTags('tasks')
 export class TasksController {
   constructor(private readonly tasksService: TasksService) {}
 
   /// GET all tasks
   @Get()
+  @ApiOperation({ summary: 'Listar todas as tasks' })
   async findAll(): Promise<Task[]> {
     return await this.tasksService.findAll();
   }
 
   /// GET one task
   @Get(':id')
+  @ApiOperation({ summary: 'Mostrar uma task especifica' })
   async findOne(@Param('id') id: number): Promise<Task> {
     const task = await this.tasksService.findOne(id);
 
@@ -34,18 +38,21 @@ export class TasksController {
 
   /// CREATE task
   @Post()
+  @ApiOperation({ summary: 'Adicionar uma nova task' })
   async create(@Body() task: Task): Promise<Task> {
     return await this.tasksService.create(task);
   }
 
   /// UPDATE task
   @Put(':id')
+  @ApiOperation({ summary: 'Atualizar os dados de uma task' })
   async update(@Param('id') id: number, @Body() task: Task): Promise<Task> {
     return this.tasksService.update(id, task);
   }
 
   /// DELETE task
   @Delete(':id')
+  @ApiOperation({ summary: 'Remover uma task' })
   async delete(@Param('id') id: number): Promise<void> {
     // handle the error if task not found
     const task = await this.tasksService.findOne(id);
